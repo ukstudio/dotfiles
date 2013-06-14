54254babdc733179a9b6ef29502d2233a9eab1b3
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2013-06-14 15:51:59 +0900

    [xmonad] setup NamedScratchpad

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -10,6 +10,7 @@ import XMonad.Layout.Magnifier
 import XMonad.Layout.IM
 import XMonad.Layout.Named
 import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Util.NamedScratchpad
 
 import Data.Monoid
 import Data.Ratio ((%))
@@ -20,6 +21,10 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 myTerminal = "gnome-terminal --hide-menubar"
+
+scratchpads = [
+    NS "htop" "gnome-terminal -t 'htop'" (title =? "htop") defaultFloating
+    ] where role = stringProperty "WM_WINDOW_ROLE"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
@@ -32,69 +37,50 @@ myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#ff0000"
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-
     -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-
     -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
-
      -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
-
     --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
     -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
-
     -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
-
     -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
-
     -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
-
     -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
-
     -- Swap the focused window and the master window
     , ((modm,               xK_Return), windows W.swapMaster)
-
     -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-
     -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-
     -- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
-
     -- Expand the master area
     , ((modm,               xK_l     ), sendMessage Expand)
-
     -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-
     -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-
     -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-
     -- Restart xmonad
     , ((modm              , xK_q     ), spawn "killall trayer;killall xscreensaver; xmonad --recompile; xmonad --restart")
-
     -- run GridSelect
     , ((modm              , xK_g     ), goToSelected defaultGSConfig)
-
     -- run shell
     , ((modm              , xK_p     ), shellPrompt defaultXPConfig)
+    -- run scratchpad
+    , ((modm .|. controlMask, xK_t   ), namedScratchpadAction scratchpads "htop")
     ]
     ++
 
@@ -183,7 +169,9 @@ main = do
             mouseBindings      = myMouseBindings,
 
             layoutHook         = myLayout,
-            manageHook         = manageDocks <+> myManageHook,
+            manageHook         = manageDocks
+                                 <+> myManageHook
+                                 <+> namedScratchpadManageHook scratchpads,
             handleEventHook    = myEventHook,
             logHook            = myLogHook xmproc,
             startupHook        = myStartupHook
