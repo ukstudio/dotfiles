c26c2f753d39b5fb60fc9357b397412db915d392
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2021-09-21 18:09:08 +0900

    Remove alacritty

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,7 +3,7 @@ include_recipe 'recipe_helper'
 include_cookbook 'functions'
 
 include_cookbook 'git'
-include_cookbook 'alacritty'
+# include_cookbook 'alacritty'
 include_cookbook 'tmux'
 include_cookbook 'yabai'
 include_cookbook 'vim'
