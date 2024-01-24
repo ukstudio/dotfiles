856e385265b9135cf0cee68f6fff41af2d2b57c9
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2024-01-24 16:13:51 +0900

    Install vim-plug

--- cookbooks/nvim/default.rb
+++ cookbooks/nvim/default.rb
@@ -2,7 +2,7 @@ package 'neovim'
 
 dotfile '.config/nvim'
 
-execute 'Install dein' do
-  command 'curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh; sh ./installer.sh ~/.cache/dein; rm installer.sh'
-  not_if 'test -d ~/.cache/dein'
+execute 'Install vim-plug' do
+  command "sh -c 'curl -fLo \"${XDG_DATA_HOME:-$HOME/.local/share}\"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
+  not_if 'test -d "${XDG_DATA_HOME:/.local/share}"/nvim/site/autoload/plug.vim'
 end
