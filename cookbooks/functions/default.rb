3da1f83694b27cf5cd084964e048344e9a2655a6
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2024-01-24 16:13:24 +0900

    Create ~/.config dir

--- cookbooks/functions/default.rb
+++ cookbooks/functions/default.rb
@@ -1,5 +1,8 @@
 define :dotfile, source: nil do
   source = params[:source] || params[:name]
+  directory File.dirname(File.join(ENV['HOME'], params[:name])) do
+    user node[:user]
+  end
   link File.join(ENV['HOME'], params[:name]) do
     to File.expand_path("../../../config/#{source}", __FILE__)
     user node[:user]
