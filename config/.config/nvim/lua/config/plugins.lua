2eca3b7f50012051dba3eabbd56906c539941e0c
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2024-03-15 19:47:15 +0900

    Lspconfig

--- config/.config/nvim/lua/config/plugins.lua
+++ config/.config/nvim/lua/config/plugins.lua
@@ -13,5 +13,29 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   "morhetz/gruvbox",
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }}
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "neovim/nvim-lspconfig",
+})
+
+require("lspconfig").ruby_ls.setup {}
+
+local cmp = require("cmp")
+cmp.setup({
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-l>"] = cmp.mapping.complete(),
+    ["<Cr>"] = cmp.mapping.confirm { select = true },
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' },
+  })
 })
